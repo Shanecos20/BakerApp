523f11abc814ae316aa0a0a8921e1f520ee5e6c2
Shanecos20 <shanecostello150@gmail.com>
2024-12-11 22:15:00 +0000

    Setup of first components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import NavigationBar from './components/NavigationBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
- 
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-
+    <div>
+      <NavigationBar />
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <h3>footer.js</h3>;
+  };
+  
+  export default Footer;
